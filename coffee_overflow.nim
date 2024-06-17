0cd9bdc9954bf96c905ec1498cd70be78d83936e
Neptendus <jlaloy@proton.me>
2024-06-17 18:32:13 +0200

    First commit

---
+++ coffee_overflow.nim
@@ -0,0 +1,69 @@
+import os, strutils, random
+
+let words = [
+  "bug", "commit", "merge", "push", "pull", "fork", "clone", "issue", "branch", 
+  "deploy", "compile", "debug", "syntax", "runtime", "variable", "function", 
+  "loop", "array", "object", "class", "module", "script", "server", "client", 
+  "api", "token", "cookie", "session", "error", "refactor"
+]
+
+let secretWord = words[rand(words.len)]
+var guessedWord = repeat('-', secretWord.len)
+var attempts = 0
+
+let coffeeStages = [
+"""
+_
+""",
+"""
+__
+""",
+"""
+___
+""",
+"""
+____
+""",
+"""
+_____
+""",
+"""
+______
+""",
+"""
+_______
+"""
+]
+#ascii art à améliorer#
+
+proc displayGame() =
+  echo coffeeStages[attempts]
+  echo "Mot: ", guessedWord
+  echo "Essais restants: ", 6 - attempts
+
+proc updateGuessedWord(letter: char) =
+  var found = false
+  for i in 0..<secretWord.len:
+    if secretWord[i] == letter:
+      guessedWord[i] = letter
+      found = true
+  if not found:
+    attempts += 1
+
+echo "Bienvenue dans Coffee Overflow !"
+
+while attempts < 6 and guessedWord != secretWord:
+  displayGame()
+  echo "Devine une lettre: "
+  let input = stdin.readLine()
+  if input.len == 1:
+    let guess = input[0]
+    updateGuessedWord(guess)
+  else:
+    echo "Veuillez entrer une seule lettre."
+
+# Fin du jeu
+if guessedWord == secretWord:
+  echo "Félicitations ! Tu as trouvé le mot : ", secretWord
+else:
+  echo "Désolé, tu as perdu. Le mot était : ", secretWord
